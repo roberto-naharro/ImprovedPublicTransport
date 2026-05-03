9c46260a653bde713805b0b8fe32a865d8836a6c
Roberto Naharro <roberto.naharro@gmail.com>
2026-05-04 01:17:33 +0200

    chore: enhance version injection logic for release builds to prioritize open PRs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Security.Permissions;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0274a35d-7e2e-47b9-8655-4ffa5c488323")]
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
